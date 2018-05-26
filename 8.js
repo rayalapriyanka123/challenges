3278e3ba0af835e4313131c7da0e22d419206f6e
Rayala Priyanka <priyanka@virtusa.com>
2018-05-26 17:16:35 +0530

    challenges

---
+++ 8.js
@@ -0,0 +1,10 @@
+
+function Calculate_Amount()
+{
+    var text=document.getElementById("tstring").value;
+    var sum=0;
+    var obj=JSON.parse(text,function(key,value)
+{
+    sum+=value;})
+    alert("total amnt="+parseInt(sum));
+}
